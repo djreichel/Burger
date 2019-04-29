e3b0d36864d21b8bed4d70945410f973631d191c
picasso <46543347+djreichel@users.noreply.github.com>
2019-04-29 13:53:53 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE books(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("double_cheeseburger", true);
+
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("plain_burger", true);
+
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("cheeseburger", true);
+
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("black_bean_burger", true);
